0b3d4fb5078400058fa44119b5bee9647996da08
Freddie <fwoods699@stowe.co.uk>
2017-06-22 06:40:41 +0100

    Revert "Revert "Handicap working""
    This reverts commit 7198cf0d7ffe4d693f9718fdc68a519e14c56079.

---
+++ GolfRecord.Model/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfRecord.Model
+{
+    public class Enums
+    {
+        public enum MatchType { StrokePlay = 1, MatchPlay = 2, Foursome = 3, StableFord = 4 }
+    }
+}
